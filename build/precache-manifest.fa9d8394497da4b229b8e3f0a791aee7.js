325963f001b283d12027647c57e8539772f69d21
unknown <1kolia1@ukr.net>
2020-06-10 13:20:25 +0300

    with change cafedra and day

---
+++ build/precache-manifest.fa9d8394497da4b229b8e3f0a791aee7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32411a5965863b94fcaefb9254910bb7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dabd328d921b2ada1d4f",
+    "url": "/static/css/main.5601b3b2.chunk.css"
+  },
+  {
+    "revision": "80ef364da7bf448e4f50",
+    "url": "/static/js/2.4894ad93.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4894ad93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dabd328d921b2ada1d4f",
+    "url": "/static/js/main.ac8da58b.chunk.js"
+  },
+  {
+    "revision": "95206017fc15c2266d5c",
+    "url": "/static/js/runtime-main.dd9f04b6.js"
+  }
+]);
